90ef81f1638f4b3480180c50f6eb80ba2510f75d
Robby Muhammad Nst <robbybellamy6@gmail.com>
2018-02-13 19:40:33 +0700

    Orbit data, and some styling on the Object Information page

--- src/app/model/NEO.ts
+++ src/app/model/NEO.ts
@@ -1,14 +1,66 @@
-export interface NEO {
+export interface LookUpModel {
+    absolute_magnitude_h: number;
+    close_approach_data: ApproachDataModel[];
+    estimated_diameter: {
+        feet: {
+            estimated_diameter_max: number;
+            estimated_diameter_min: number;
+        },
+        kilometers: {
+            estimated_diameter_min: number;
+            estimated_diameter_max: number;
+        },
+        meters: {
+            estimated_diameter_min: number;
+            estimated_diameter_max: number;
+        },
+        miles: {
+            estimated_diameter_min: number;
+            estimated_diameter_max: number;
+        }
+    };
+    is_potentially_hazardous_asteroid: boolean;
+    name: string;
+    neo_reference_id: string;
+    nasa_jpl_url: string;
+    orbital_data: OrbitalDataModel;
+}
+
+
+export interface ApproachDataModel {
     miss_distance: {
         astronomical: string;
         lunar: string;
         kilometers: string;
+        miles: string;
     };
-    orbiting_body: {
-        kilometers_per_second: string;
+    relative_velocity: {
         kilometers_per_hour: string;
+        kilometers_per_second: string;
         miles_per_hour: string;
     };
-    relative_velocity: string;
+    orbiting_body: string;
     close_approach_date: string;
+    epoch_date_close_approach: number;
+}
+
+export interface OrbitalDataModel {
+    aphelion_distance: string;
+    ascending_node_longtitude: string;
+    eccentricity: string;
+    epoch_osculation: string;
+    equinox: string;
+    inclination: string;
+    jupiter_tisserand_invariant: string;
+    mean_anomaly: string;
+    mean_motion: string;
+    minimum_orbit_intersection: string;
+    orbit_determination_date: string;
+    orbit_id: string;
+    orbit_uncertainty: string;
+    orbital_period: string;
+    perihelion_argument: string;
+    perihelion_distance: string;
+    perihelion_time: string;
+    semi_major_axis: string;
 }
--- src/app/neo-information/neo-information.component.ts
+++ src/app/neo-information/neo-information.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { MatTableDataSource, MatPaginator } from '@angular/material';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { NasaService } from '../services/nasa.service';
+import { LookUpModel, ApproachDataModel } from '../model/NEO';
 
 @Component({
   selector: 'app-neo-information',
@@ -11,11 +12,14 @@ import { NasaService } from '../services/nasa.service';
 export class NeoInformationComponent implements OnInit, AfterViewInit {
 
   displayedColumns = ['No', 'Orbiting Body', 'Date', 'Miss Distance'];
-  dataSource = new MatTableDataSource<any>();
+  neo_data: LookUpModel;
+  dataSource = new MatTableDataSource<ApproachDataModel>();
   sub: any;
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
-  constructor( private _activeRoute: ActivatedRoute, private _nasaService: NasaService ) {
+  constructor( private _activeRoute: ActivatedRoute,
+    private _nasaService: NasaService,
+    private _router: Router ) {
     this.sub = this._activeRoute.params.subscribe(
       params => {
         this.getData(params['id']);
@@ -30,10 +34,12 @@ export class NeoInformationComponent implements OnInit, AfterViewInit {
     this._nasaService.getNEOLookUp(param)
       .subscribe(
         data => {
-          const json_data = JSON.parse(data.text());
-          this.dataSource = new MatTableDataSource<any>(json_data.close_approach_data);
+          this.neo_data = JSON.parse(data.text());
+          this.dataSource = new MatTableDataSource<ApproachDataModel>(this.neo_data.close_approach_data);
           this.dataSource.paginator = this.paginator;
-          console.log(this.dataSource);
+        },
+        err => {
+          console.log(err);
         }
       );
   }
@@ -48,33 +54,3 @@ export class NeoInformationComponent implements OnInit, AfterViewInit {
   }
 
 }
-
-export interface Element {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: Element[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  {position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'},
-  {position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
-  {position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
-  {position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si'},
-  {position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
-  {position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S'},
-  {position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'},
-  {position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'},
-  {position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'},
-  {position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'},
-];
